3361eda57eda347fdfeb3c4185afcfe9fe46c90a
OtariOboladze <56553623+OtariOboladze@users.noreply.github.com>
2020-04-14 18:33:17 +0200

    amejorado 14/04/2020

--- starter-code/js/script.js
+++ starter-code/js/script.js
@@ -1,42 +1,75 @@
-const deleteItem = (e) =>{
+function calculatePrices() {
+      let precioUnitario = parseInt(
+            document.getElementById("price").innerHTML.substring(1)
+      );
+      let cantidad = parseInt(document.getElementById("qtyInput").value);
 
+      let precioUnitario2 = parseInt(
+            document.getElementById("price2").innerHTML.substring(1)
+      );
+      let cantidad2 = parseInt(document.getElementById("qtyInput2").value);
+
+      document.getElementById("totalPrice").innerHTML =
+            "$ " + (precioUnitario * cantidad).toFixed(2) ;
+
+      document.getElementById("totalPrice2").innerHTML =
+            "$ " + (precioUnitario2 * cantidad2).toFixed(2) ;
+
+      let first = (precioUnitario * cantidad).toFixed(2);
+      let second = (precioUnitario2 * cantidad2).toFixed(2);
+      document.querySelector("h2").firstElementChild.innerText =
+            "$ " + (parseInt(first) + parseInt(second));
 }
 
-const getPriceByProduct = (itemNode) =>{
+document.getElementById("calcButton").onclick = calculatePrices;
 
+// Eliminando un elemento específico cuando se conoce su nodo padre
+// var d = document.getElementById("top");
+// var d_nested = document.getElementById("anidados");
+// var throwawayNode = d.removeChild(d_nested);
+
+var selectedDelete = document.querySelectorAll("#delete, #delete2");
+
+function deleteProduct() {
+      let hijoElemento = document.querySelector(".container");
+      let padreElemento = document.querySelector("#body");
+      console.log("bliad raiyo");
+	return padreElemento.removeChild(hijoElemento);
+      
 }
 
-const updatePriceByProduct = (productPrice, index) =>{
-
+function deleteProduct2() {
+      let hijoElemento2 = document.querySelector(".container2");
+      let padreElemento = document.querySelector("#body");
+      console.log("bliad raiyo2");
+      return padreElemento.removeChild(hijoElemento2);
 }
 
-const getTotalPrice = ()  =>{
+selectedDelete[0].onclick = deleteProduct;
+selectedDelete[1].onclick = deleteProduct2;
 
-}
 
-const createQuantityInput = () =>{
 
-}
 
-const createDeleteButton = () =>{
 
-}
+const deleteItem = (e) => {};
 
-const createQuantityNode = () =>{
+const getPriceByProduct = (itemNode) => {};
 
-}
+const updatePriceByProduct = (productPrice, index) => {};
 
-const createItemNode = (dataType, itemData) =>{
+const getTotalPrice = () => {};
 
-}
+const createQuantityInput = () => {};
 
-const createNewItemRow = (itemName, itemUnitPrice) =>{
+const createDeleteButton = () => {};
 
-}
+const createQuantityNode = () => {};
 
-const createNewItem = () =>{
+const createItemNode = (dataType, itemData) => {};
 
-}
+const createNewItemRow = (itemName, itemUnitPrice) => {};
 
-addEventListener = ('load',  ()=>{
-});
+const createNewItem = () => {};
+
+addEventListener = ("load", () => {});
