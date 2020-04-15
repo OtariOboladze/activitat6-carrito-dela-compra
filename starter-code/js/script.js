96989c873ef6fd91af28574daec9b9db37bccb37
OtariOboladze <56553623+OtariOboladze@users.noreply.github.com>
2020-04-15 08:33:37 +0200

    amejorado 14/04/2020

--- starter-code/js/script.js
+++ starter-code/js/script.js
@@ -1,54 +1,28 @@
-function calculatePrices() {
-      let precioUnitario = parseInt(
-            document.getElementById("price").innerHTML.substring(1)
-      );
-      let cantidad = parseInt(document.getElementById("qtyInput").value);
 
-      let precioUnitario2 = parseInt(
-            document.getElementById("price2").innerHTML.substring(1)
-      );
-      let cantidad2 = parseInt(document.getElementById("qtyInput2").value);
 
-      document.getElementById("totalPrice").innerHTML =
-            "$ " + (precioUnitario * cantidad).toFixed(2) ;
-
-      document.getElementById("totalPrice2").innerHTML =
-            "$ " + (precioUnitario2 * cantidad2).toFixed(2) ;
-
-      let first = (precioUnitario * cantidad).toFixed(2);
-      let second = (precioUnitario2 * cantidad2).toFixed(2);
-      document.querySelector("h2").firstElementChild.innerText =
-            "$ " + (parseInt(first) + parseInt(second));
-}
+// CREATE PRODUCT
 
 document.getElementById("calcButton").onclick = calculatePrices;
+function calculatePrices() {
+      var totalPrice = 0;
+      for (let i = 0; i < document.getElementsByClassName("container").length; i++) {
+            let precioUnitario = parseInt(document.getElementsByClassName("price")[i].innerHTML.substring(1));
+            let cantidad = parseInt(document.getElementsByClassName("qtyInput")[i].value);
 
-// Eliminando un elemento específico cuando se conoce su nodo padre
-// var d = document.getElementById("top");
-// var d_nested = document.getElementById("anidados");
-// var throwawayNode = d.removeChild(d_nested);
-
-var selectedDelete = document.querySelectorAll("#delete, #delete2");
-
-function deleteProduct() {
-      let hijoElemento = document.querySelector(".container");
-      let padreElemento = document.querySelector("#body");
-      console.log("bliad raiyo");
-	return padreElemento.removeChild(hijoElemento);
-      
-}
-
-function deleteProduct2() {
-      let hijoElemento2 = document.querySelector(".container2");
-      let padreElemento = document.querySelector("#body");
-      console.log("bliad raiyo2");
-      return padreElemento.removeChild(hijoElemento2);
-}
-
-selectedDelete[0].onclick = deleteProduct;
-selectedDelete[1].onclick = deleteProduct2;
+            let x = (precioUnitario * cantidad).toFixed(2);
+            document.getElementsByClassName("productPrice")[i].innerHTML = "$ " + x;
+            totalPrice += parseInt(x);
+      }
+      document.querySelector("h2").firstElementChild.innerText = "$ " + totalPrice;
+};
 
 
+// DELETE PRODUCT
+
+for (let i = 0; i < document.getElementsByClassName("container").length; i++) {
+	let x = document.getElementsByClassName("container")[i];
+	document.querySelectorAll(".delete")[i].onclick = function(){x.parentNode.removeChild(x)};
+};
 
 
 
